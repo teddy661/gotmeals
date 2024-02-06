6847c4bb2a561fe5bd5ff68afe14444e96fc5c30
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-02-05 20:22:25 -0800

    dump a pickle file

--- src/doit.py
+++ src/doit.py
@@ -1,11 +1,19 @@
-import matplotlib.pyplot as plt
-from PIL import Image
-import numpy as np
-matplotlib.use('TkAgg')
+from pathlib import Path
 
-pil = Image.open("IMG_0118.PNG")
+import pickle
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+
+matplotlib.use("TkAgg")
+
+img_path = Path("../data/sample_data/IMG_0119.PNG")
+pil = Image.open(img_path)
 im = np.asarray(pil)
 plt.imshow(im)
 
-pts = plt.ginput(10) #number of clicks
+pts = plt.ginput(4)  # number of clicks
+with open("points.pkl", "wb") as f:
+    pickle.dump(pts, f, pickle.HIGHEST_PROTOCOL)
 print(pts)
