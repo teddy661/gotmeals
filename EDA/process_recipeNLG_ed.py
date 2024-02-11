6ad6d2ecb380699a675c5e583a355c74caf6fe60
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-02-11 08:00:45 -0800

    convert csv to parquet

---
+++ EDA/process_recipeNLG_ed.py
@@ -0,0 +1,90 @@
+import ast
+import logging
+import multiprocessing as mp
+import platform
+from pathlib import Path
+
+import numpy as np
+import polars as pl
+import psutil
+from platformdirs import user_documents_dir
+
+from utils import parallelize_dataframe
+
+
+def clean_text(text: str) -> str:
+    """
+    Clean the text by removing any non-alphabetic characters and converting to lower case
+    """
+    return " ".join([word.lower() for word in text.split() if word.isalpha()])
+
+
+def main():
+    # Setup the directories
+    documents_dir = Path(user_documents_dir())
+    if platform.system() == "Windows":
+        class_root_dir = documents_dir.joinpath("01-Berkeley/210")
+        project_root_dir = class_root_dir.joinpath("gotmeals")
+        data_root_dir = class_root_dir.joinpath("data")
+    elif platform.system() == "Linux":
+        class_root_dir = Path("/tf/notebooks")
+        project_root_dir = class_root_dir.joinpath("gotmeals")
+        data_root_dir = class_root_dir.joinpath("data")
+
+    rnlg_data_dir = data_root_dir.joinpath("RecipeDatabase")
+    rnlg_csv_file = rnlg_data_dir.joinpath("RecipeNLG_dataset.csv")
+    rnlg_parquet_file = rnlg_data_dir.joinpath("RecipeNLG_dataset.parquet")
+    # Load the RecipeNLG dataset
+    rnlg_df = pl.read_csv(rnlg_csv_file)
+
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        level=logging.INFO,
+    )
+    num_cpus = psutil.cpu_count(logical=False)
+    ## Fix the '' column name to id
+    rnlg_df = rnlg_df.rename({"": "id"})
+
+    ## Fix the columns into lists from strings
+    column_names_convert_to_list = ["ingredients", "directions", "NER"]
+    for col_name in column_names_convert_to_list:
+        logging.info(f"Converting {col_name} to list")
+        rnlg_df = rnlg_df.with_columns(
+            pl.col(col_name)
+            .map_elements(lambda x: ast.literal_eval(x))
+            .alias(col_name),
+        )
+
+    ## convert all ingredients to lower case and remove leading and trailing spaces for NER
+    ## do not do this conversion for ingredients column
+    ## also add a column for the length of the list
+    for col_name in ["ingredients", "NER"]:
+        if col_name == "NER":
+            logging.info(f"Fixing Strings:\t{col_name}")
+            rnlg_df = rnlg_df.with_columns(
+                pl.col(col_name)
+                .map_elements(
+                    lambda lst: [
+                        " ".join([word.lower() for word in s.split() if word.isalpha()])
+                        for s in lst
+                    ]
+                )
+                .alias(col_name),
+            )
+        logging.info(f"Adding Counts:\t{col_name}")
+        rnlg_df = rnlg_df.with_columns(
+            pl.col(col_name)
+            .map_elements(lambda lst: len(lst))
+            .alias(col_name + "_len"),
+        )
+
+    logging.info(f"Writing parquet:\t{rnlg_parquet_file}")
+    rnlg_df.write_parquet(
+        rnlg_parquet_file, compression="zstd", compression_level=9, statistics=True
+    )
+
+
+if __name__ == "__main__":
+    mp.freeze_support()
+    mp.set_start_method("spawn")
+    main()
