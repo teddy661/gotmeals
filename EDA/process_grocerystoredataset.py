e6c944d9ab4779d837e213ea390c8d33c19c4880
Ed Brown <teddybrown@ischool.berkeley.edu>
2024-02-12 10:29:27 -0800

    minor updates

--- EDA/process_grocerystoredataset.py
+++ EDA/process_grocerystoredataset.py
@@ -2,7 +2,6 @@ import argparse
 import gc
 import logging
 import multiprocessing as mp
-import platform
 import sys
 from datetime import datetime
 from io import BytesIO
@@ -14,25 +13,13 @@ import numpy as np
 import polars as pl
 import psutil
 from PIL import Image
-from platformdirs import user_documents_dir
 
-from utils import convert_numpy_to_bytesio, parallelize_dataframe
+from utils import ProjectConfig, convert_numpy_to_bytesio, parallelize_dataframe
 
-environment = platform.system()
-# Setup the directories
-documents_dir = Path(user_documents_dir())
-if platform.system() == "Windows":
-    class_root_dir = documents_dir.joinpath("01-Berkeley/210")
-    project_root_dir = class_root_dir.joinpath("gotmeals")
-    data_root_dir = class_root_dir.joinpath("data")
-elif platform.system() == "Linux":
-    class_root_dir = Path("/tf/notebooks")
-    project_root_dir = class_root_dir.joinpath("gotmeals")
-    data_root_dir = class_root_dir.joinpath("data")
+pc = ProjectConfig()
 
 
-DATA_ROOT = class_root_dir.joinpath("data")
-CLASSIFICATION_ROOT = DATA_ROOT.joinpath(
+CLASSIFICATION_ROOT = pc.data_root_dir.joinpath(
     "GroceryStoreDataset/GroceryStoreDataset/dataset"
 )
 
@@ -86,7 +73,9 @@ def main():
         logging.error(f"Directory {CLASSIFICATION_ROOT} does not exist.")
     TRAIN_IMG_DIR = CLASSIFICATION_ROOT.joinpath("dataset/train")
 
-    num_cpus = 8
+    num_cpus = psutil.cpu_count(logical=False)
+    if num_cpus > 10:
+        num_cpus = 10
 
     train_df = pl.read_csv(CLASSIFICATION_ROOT.joinpath("train.txt"), has_header=False)
     train_df = train_df.rename(
@@ -106,7 +95,9 @@ def main():
     )
     df = parallelize_dataframe(df, read_image_wrapper, num_cpus)
     print(df.head())
-    df.write_parquet("grocerystoredataset.parquet", compression="snappy")
+    df.write_parquet(
+        pc.data_root_dir.joinpath("GroceryStoreDataset.parquet"), compression="snappy"
+    )
 
 
 if __name__ == "__main__":
