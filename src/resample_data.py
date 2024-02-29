2d85d704800b629134d78572370cee9bb40d8439
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-02-28 17:18:11 -0800

    for now to deal with class imblance write a tool that will oversample all the data to equal levels. this does not really help classes where we have low numbers, but it does make model loss and accuracy somewhat more meaningful. need to finish

---
+++ src/resample_data.py
@@ -0,0 +1,55 @@
+import polars as pl
+from pathlib import Path
+import multiprocessing as mp
+from platformdirs import user_documents_dir
+import platform
+
+SAMPLES_PER_CLASS = 220
+RANDOM_SEED = 42
+TRAIN_PERCENTAGE = 0.8
+
+class ProjectConfig:
+    def __init__(self):
+        self.system = platform.system()
+        self.user_documents_dir = Path(user_documents_dir())
+        if platform.system() == "Windows":
+            self.class_root_dir = self.user_documents_dir.joinpath("01-Berkeley/210")
+            self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
+            self.data_root_dir = self.class_root_dir.joinpath("data")
+        elif platform.system() == "Linux":
+            self.class_root_dir = Path("/tf/notebooks")
+            self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
+            self.data_root_dir = self.class_root_dir.joinpath("data")
+
+
+def get_sampled_data(raw_train_df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Samples the dataframe to get the same number of samples per class our smallest class
+    has 210 examples so we'll use that as our sample size. We'll also shuffle the dataframe
+    :param df: The dataframe to sample
+    :return: The sampled dataframe
+    """
+    pl.set_random_seed(RANDOM_SEED)
+    train_equal_sample_df = pl.concat(
+        [
+            x.sample(SAMPLES_PER_CLASS, with_replacement=True, shuffle=False)
+            for x in raw_train_df.partition_by("ClassId")
+        ]
+    )
+    return train_equal_sample_df.sample(shuffle=True, fraction=1)
+
+def main():
+    #Blindly oversample the data to ensure consistent class distribution
+    #Load the data
+    pc = ProjectConfig()
+    source_data = pc.data_root_dir.joinpath("extracted_common_images.parquet")
+    sampled_data = get_sampled_data(pl.read_parquet(source_data)
+    target_dir = pc.data_root_dir.joinpath("sampled_data")
+    if not target_dir.exists():
+        target_dir.mkdir()
+    
+
+if __name__ == "__main__":
+    mp.freeze_support()
+    mp.set_start_method("spawn")
+    main()
