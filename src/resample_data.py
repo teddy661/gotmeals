f1597a59ce462142cd51be664b543180a4677734
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-02-28 22:08:45 -0800

    finish resample_data blindly samples until class sizes are the same. rely on pipeline augmentations to make more sense of this

--- src/resample_data.py
+++ src/resample_data.py
@@ -1,7 +1,10 @@
 import multiprocessing as mp
 import platform
+import shutil
+import uuid
 from pathlib import Path
 
+import numpy as np
 import polars as pl
 from platformdirs import user_documents_dir
 
@@ -14,30 +17,73 @@ TRAIN_PERCENTAGE = 0.8
 
 def get_sampled_data(raw_train_df: pl.DataFrame) -> pl.DataFrame:
     """
-    Samples the dataframe to get the same number of samples per class our smallest class
-    has 210 examples so we'll use that as our sample size. We'll also shuffle the dataframe
+    Oversample our data to a median class size of 220 samples if the class has more than SAMPLES_PER_CLASS
+    samples, then sample without replacement if it has less, sample with replacement.
     :param df: The dataframe to sample
     :return: The sampled dataframe
     """
     pl.set_random_seed(RANDOM_SEED)
     train_equal_sample_df = pl.concat(
         [
-            x.sample(SAMPLES_PER_CLASS, with_replacement=True, shuffle=False)
+            (
+                x.sample(SAMPLES_PER_CLASS, with_replacement=True, shuffle=False)
+                if x.height <= SAMPLES_PER_CLASS
+                else x.sample(SAMPLES_PER_CLASS, with_replacement=False, shuffle=False)
+            )
             for x in raw_train_df.partition_by("ClassId")
         ]
     )
-    return train_equal_sample_df.sample(shuffle=True, fraction=1)
+    return train_equal_sample_df
+
+
+def duplicate_image(target_dir: str, class_id: str, image_path: str) -> pl.DataFrame:
+    """ """
+    target_dir = Path(target_dir)
+    src_image_path = Path(image_path)
+    src_image_base_name = src_image_path.stem
+    src_image_suffix = src_image_path.suffix
+    random_append = uuid.uuid4().hex
+    image_dir = target_dir.joinpath(class_id)
+    image_dir.mkdir(parents=True, exist_ok=True)
+    target_image_path = image_dir.joinpath(
+        src_image_base_name + random_append + src_image_suffix
+    )
+    shutil.copy(src_image_path, target_image_path)
+    return str(target_image_path)
+
+
+def create_sampled_data(df: pl.DataFrame) -> pl.DataFrame:
+    df = df.with_columns(
+        pl.struct(["target_dir", "ClassId", "Image_Path"])
+        .map_elements(
+            lambda x: duplicate_image(x["target_dir"], x["ClassId"], x["Image_Path"]),
+        )
+        .alias("Sampled_Image_Path")
+    )
+    return df
 
 
 def main():
     # Blindly oversample the data to ensure consistent class distribution
+    # we can be smarter later on
     # Load the data
     pc = ProjectConfig()
-    source_data = pc.data_root_dir.joinpath("extracted_common_images.parquet")
+    target_name = "sampled_training_data"
+    target_dir = pc.data_root_dir.joinpath(target_name)
+
+    source_data = pc.data_root_dir.joinpath("training_data.parquet")
     sampled_data = get_sampled_data(pl.read_parquet(source_data))
-    target_dir = pc.data_root_dir.joinpath("sampled_data")
-    if not target_dir.exists():
-        target_dir.mkdir()
+    sampled_data = sampled_data.with_columns(
+        pl.lit(str(target_dir)).alias("target_dir")
+    )
+
+    target_dir.mkdir(parents=True, exist_ok=True)
+    target_parquet = pc.data_root_dir.joinpath(target_name + ".parquet")
+
+    sampled_data = parallelize_dataframe(sampled_data, create_sampled_data, 8)
+    sampled_data = sampled_data.drop("target_dir")
+
+    sampled_data.write_parquet(target_parquet)
 
 
 if __name__ == "__main__":
