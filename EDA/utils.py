710d20f62790c62cd4a15b73ad5cc44dff52fee4
Ed Brown <edbrown@berkeley.edu>
2024-02-29 11:43:40 -0800

    configuration file overrides for ProjectConfig

--- EDA/utils.py
+++ EDA/utils.py
@@ -1,4 +1,5 @@
 import itertools
+import json
 import multiprocessing as mp
 import os
 import platform
@@ -117,11 +118,68 @@ class ProjectConfig:
     def __init__(self):
         self.system = platform.system()
         self.user_documents_dir = Path(user_documents_dir())
-        if platform.system() == "Windows":
-            self.class_root_dir = self.user_documents_dir.joinpath("01-Berkeley/210")
-            self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
-            self.data_root_dir = self.class_root_dir.joinpath("data")
-        elif platform.system() == "Linux":
-            self.class_root_dir = Path("/tf/notebooks")
-            self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
-            self.data_root_dir = self.class_root_dir.joinpath("data")
+        self.configuration_file = Path(user_documents_dir()).joinpath(
+            "gotmeals_config.json"
+        )
+        if self.config_exists():
+            print("Configuration file found. Using settings from file.")
+            self.read_config()
+        else:
+            print("No configuration file found. Using defaults.")
+            if platform.system() == "Windows":
+                self.class_root_dir = self.user_documents_dir.joinpath(
+                    "01-Berkeley/210"
+                )
+                self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
+                self.data_root_dir = self.class_root_dir.joinpath("data")
+            elif platform.system() == "Linux":
+                self.class_root_dir = Path("/tf/notebooks")
+                self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
+                self.data_root_dir = self.class_root_dir.joinpath("data")
+
+    def config_exists(self):
+        if not self.configuration_file.exists():
+            return False
+        else:
+            return True
+
+    def read_config(self):
+        try:
+            with open(self.configuration_file, "r") as f:
+                config = json.load(f)
+        except json.JSONDecodeError as e:
+            print("Error: JSON decoding failed. Reason:", e)
+            config_scuccessful = False
+        else:
+            class_root_dir = Path(config["class_root_dir"])
+            if class_root_dir.exists():
+                self.class_root_dir = class_root_dir
+                print(f"Class root configured from file: {self.class_root_dir}")
+            else:
+                if platform.system() == "Windows":
+                    self.class_root_dir = self.user_documents_dir.joinpath(
+                        "01-Berkeley/210"
+                    )
+                elif platform.system() == "Linux":
+                    self.class_root_dir = Path("/tf/notebooks")
+                print(f"Class root not found. Using default: {self.class_root_dir}")
+            project_root_dir = Path(config["project_root_dir"])
+            if project_root_dir.exists():
+                self.project_root_dir = project_root_dir
+                print(f"Project root configured from file. {self.project_root_dir}")
+            else:
+                if platform.system() == "Windows":
+                    self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
+                elif platform.system() == "Linux":
+                    self.project_root_dir = self.class_root_dir.joinpath("gotmeals")
+                print(f"Project root not found. Using default: {self.project_root_dir}")
+            data_root_dir = Path(config["data_root_dir"])
+            if data_root_dir.exists():
+                self.data_root_dir = data_root_dir
+                print(f"Data root configured from file. {self.data_root_dir}")
+            else:
+                if platform.system() == "Windows":
+                    self.data_root_dir = self.class_root_dir.joinpath("data")
+                elif platform.system() == "Linux":
+                    self.data_root_dir = self.class_root_dir.joinpath("data")
+                print(f"Data root not found. Using default: {self.data_root_dir}")
