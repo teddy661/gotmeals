06bbcf3eb33029b1fad8750df65f8ae8f4170cda
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-03-02 13:59:16 -0800

    scrape images from google when/if things look bad

---
+++ image-scraping/createjson.py
@@ -0,0 +1,161 @@
+import json
+from collections import OrderedDict
+
+data = [
+    {"ClassId": "rice", "SearchStrings": ["bag of white rice", "box of rice"]},
+    {
+        "ClassId": "red wine",
+        "SearchStrings": ["red wine", "red wine bottle", "red wine bottle table"],
+    },
+    {"ClassId": "corn", "SearchStrings": ["corn", "corn on the cob", "raw corn"]},
+    {
+        "ClassId": "cabbage",
+        "SearchStrings": ["cabbage", "cabbage leaves", "raw cabbage"],
+    },
+    {"ClassId": "egg", "SearchStrings": ["eggs", "eggs in shells", "eggs in carton"]},
+    {
+        "ClassId": "bell pepper",
+        "SearchStrings": ["bell pepper", "raw bell pepper", "fresh bell peppers"],
+    },
+    {
+        "ClassId": "cream cheese",
+        "SearchStrings": [
+            "cream cheese",
+            "cream cheese box",
+            "philadelphia cream cheese",
+            "philadelphia cream cheese container",
+        ],
+    },
+    {"ClassId": "cream", "SearchStrings": ["heavy whipping cream", "heavy cream"]},
+    {
+        "ClassId": "bread crumbs",
+        "SearchStrings": [
+            "bread crumb packaging",
+            "bread crumbs packaging",
+        ],
+    },
+    {
+        "ClassId": "mushroom soup",
+        "SearchStrings": ["mushroom soup can", "mushroom soup cans"],
+    },
+    {
+        "ClassId": "cream ofchicken soup",
+        "SearchStrings": ["cream of chicken soup cans", "cream of chicken soup can"],
+    },
+    {
+        "ClassId": "tomato paste",
+        "SearchStrings": ["tomato paste can", "tomato paste cans"],
+    },
+    {
+        "ClassId": "sesame oil",
+        "SearchStrings": ["sesame oil", "sesame oil bottle", "sesame oil bottles"],
+    },
+    {
+        "ClassId": "buttermilk",
+        "SearchStrings": [
+            "buttermilk container",
+            "buttermilk carton",
+            "buttermilk cartons",
+        ],
+    },
+    {
+        "ClassId": "black beans",
+        "SearchStrings": ["black beans can", "black beans cans"],
+    },
+    {"ClassId": "chives", "SearchStrings": ["fresh chives", "fresh chives in grocery"]},
+    {
+        "ClassId": "salsa",
+        "SearchStrings": ["jar of salsa", "jars of salsa", "salsa jar in grocery"],
+    },
+    {
+        "ClassId": "cottage cheese",
+        "SearchStrings": ["cottage cheese container", "cottage cheese containers"],
+    },
+    {
+        "ClassId": "onion soup",
+        "SearchStrings": [
+            "onion soup can",
+            "onion soup cans",
+            "onion soup packet",
+            "onion soup packaging",
+        ],
+    },
+    {
+        "ClassId": "capers",
+        "SearchStrings": [
+            "jar of capers",
+            "jars of capers",
+            "jars of capers",
+            "capers in grocery store",
+        ],
+    },
+    {
+        "ClassId": "water chestnuts",
+        "SearchStrings": [
+            "water chestnuts can",
+            "water chestnuts cans",
+            "water chestnuts can grocery",
+        ],
+    },
+    {
+        "ClassId": "horseradish",
+        "SearchStrings": [
+            "horseradish jar",
+            "horseradish jars",
+            "horseradish jars grocery",
+        ],
+    },
+    {
+        "ClassId": "sherry",
+        "SearchStrings": [
+            "cooking sherry",
+            "cooking sherry grocery",
+            "cooking sherry bottle",
+        ],
+    },
+    {
+        "ClassId": "molasses",
+        "SearchStrings": ["molasses jar", "molasses jars", "molasses jar grocery"],
+    },
+    {"ClassId": "tuna", "SearchStrings": ["tuna can", "tuna cans", "tuna can grocery"]},
+    {
+        "ClassId": "pork chops",
+        "SearchStrings": ["pork chops in packaging", "pork chops in packaging grocery"],
+    },
+    {
+        "ClassId": "active dry yeast",
+        "SearchStrings": [
+            "active dry yeast",
+            "active dry yeast packets",
+            "active dry yeast jar",
+        ],
+    },
+    {
+        "ClassId": "cornmeal",
+        "SearchStrings": ["cornmeal box", "cornmeal box grocery", "cornmeal package"],
+    },
+    #
+    #    Above are good
+    #    { "ClassId": "chicken", "SearchStrings": ["chicken", "chicken breast", "raw chicken"]},
+    #    { "ClassId": "onion", "SearchStrings": ["onion", "onions", "raw onions"]},
+    #    { "ClassId": "garlic", "SearchStrings": ["garlic", "garlic cloves", "raw garlic"]},
+    #    { "ClassId": "tomato", "SearchStrings": ["tomato", "tomatoes", "raw tomatoes"]},
+    #    { "ClassId": "potato", "SearchStrings": ["potato", "potatoes", "raw potatoes"]},
+    #    { "ClassId": "bell_pepper", "SearchStrings": ["bell pepper", "bell peppers", "raw bell peppers"]},
+    #    { "ClassId": "lettuce", "SearchStrings": ["lettuce", "lettuce leaves", "raw lettuce"]},
+    #    { "ClassId": "spinach", "SearchStrings": ["spinach", "spinach leaves", "raw spinach"]},
+    #    { "ClassId": "kale", "SearchStrings": ["kale", "kale leaves", "raw kale"]},
+    #    { "ClassId": "broccoli", "SearchStrings": ["broccoli", "broccoli florets", "raw broccoli"]},
+    #    { "ClassId": "cauliflower", "SearchStrings": ["cauliflower", "cauliflower florets", "raw cauliflower"]},
+    #    { "ClassId": "asparagus", "SearchStrings": ["asparagus", "asparagus spears", "raw asparagus"]},
+    #   { "ClassId": "eggplant", "SearchStrings": ["eggplant", "eggplant slices", "raw eggplant"]},
+    #   { "ClassId": "mushroom", "SearchStrings": ["mushroom", "mushrooms", "raw mushrooms"]},
+    #   { "ClassId": "green_beans", "SearchStrings": ["green beans", "green beans", "raw green beans"]},
+    #    { "ClassId": "green_peas", "SearchStrings": ["green peas", "green peas", "raw green peas"]},
+    #    { "ClassId": "green_onion", "SearchStrings": ["green onion", "green onions", "raw green onions"]},
+]
+
+json_data = json.dumps(data, indent=2)
+with open("queries.json", "w") as f:
+    f.write(json_data)
+    f.close()
---
+++ image-scraping/scrape-ingredients.py
@@ -0,0 +1,168 @@
+import json
+import pickle
+import random
+import time
+from io import BytesIO
+from pathlib import Path
+
+import polars as pl
+import psutil
+import requests
+import urllib3
+import validators
+from blake3 import blake3
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+from utils import *
+
+
+def download_images(url_list: list, class_dir: Path) -> list:
+    user_agent = {
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
+    http = urllib3.PoolManager(8, headers=user_agent)
+    if not class_dir.exists():
+        class_dir.mkdir(parents=True)
+
+    image_paths = []
+    for i, url in enumerate(url_list, 1):
+        if not validators.url(url):
+            image_path = None
+        else:
+            time.sleep(random.uniform(0.2, 1.54))
+            response = http.request("GET", url)
+            if response.status != 200:
+                print(f"Failed to download image {i} from {url}")
+            else:
+                image_data = response.data
+                image_hash = blake3(image_data).hexdigest()
+                image_path = class_dir.joinpath(f"{image_hash}.jpg")
+                with open(image_path, "wb") as f:
+                    f.write(image_data)
+        path_to_append = str(image_path) if image_path is not None else None
+        image_paths.append(path_to_append)
+    return image_paths
+
+
+def main():
+    target_directory_name = "image_downloads"
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    target_directory = Path(f"image_downloads_{timestr}").resolve()
+    if not target_directory.exists():
+        target_directory.mkdir(parents=True)
+
+    QUERY_FILE = Path("queries.json").resolve()
+    with open(QUERY_FILE, "r") as f:
+        queries = json.load(f)
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--disable-extensions")
+
+    query_results = []
+    for c in queries:
+        class_id = c["ClassId"]
+        search_strings = c["SearchStrings"]
+        print(f"Processing class     :\t{class_id}")
+        driver = webdriver.Chrome(
+            service=ChromeService(
+                ChromeDriverManager().install(), options=chrome_options
+            )
+        )
+        for search_string in search_strings:
+            print(f"\tSearch String:\t{search_string}")
+            # Create url variable containing the webpage for a Google image search.
+            # url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568")
+            # url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&bih=568")
+            url = "https://www.google.com/search?q={s}&sca_esv=f7010a6fa6b14553&tbm=isch&source=hp&biw=1969&bih=1058&ei=xGniZaLeHJfbkPIPqrCZ-A8&iflsig=ANes7DEAAAAAZeJ31G8Uz-MkGwSj2hY82lWUOb-ewC7m&ved=0ahUKEwii0IjToNSEAxWXLUQIHSpYBv8Q4dUDCAc&uact=5&oq=celery&gs_lp=EgNpbWciBmNlbGVyeTIIEAAYgAQYsQMyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgAQyBRAAGIAEMgUQABiABDIFEAAYgARIkAhQAFjJBnAAeACQAQCYAS-gAYACqgEBNrgBA8gBAPgBAYoCC2d3cy13aXotaW1nmAIGoAKMApgDAJIHATY&sclient=img"
+            # url = "https://www.google.com/search?q={s}&tbm=isch"
+            # Launch the browser and open the given url in the webdriver.
+            driver.get(url.format(s=search_string))
+            # Scroll down the body of the web page and load the images.
+            SCROLL_PAUSE_TIME = random.uniform(2.0, 3.0)
+
+            # Get scroll height
+            last_height = driver.execute_script("return document.body.scrollHeight")
+
+            while True:
+                # Scroll down to bottom
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                # Wait to load page
+                time.sleep(SCROLL_PAUSE_TIME)
+
+                # Calculate new scroll height and compare with last scroll height
+                new_height = driver.execute_script("return document.body.scrollHeight")
+
+                if new_height == last_height:
+                    # break #insert press load more
+                    try:
+                        element = driver.find_elements(
+                            By.CLASS_NAME, "LZ4I"
+                        )  # returns list
+                        element[0].click()
+                    except:
+                        break
+                last_height = new_height
+
+            # Find the images.
+            # imgResults = driver.find_elements(By.XPATH, "//img[contains(@class,'Q4LuWd')]")
+            image_links = driver.find_elements(By.CLASS_NAME, "rg_i.Q4LuWd")
+
+            # Access and store the scr list of image url's.
+            src_links = [img.get_attribute("src") for img in image_links]
+            data_src_links = [img.get_attribute("data-src") for img in image_links]
+
+            # combine and clean the links
+            combined_links = src_links + data_src_links
+            final_links = list(filter(lambda x: x is not None, combined_links))
+            # Retrieve and download the images.
+            written_files = download_images(
+                final_links, target_directory.joinpath(class_id)
+            )
+            # OrderedDict([('ClassId', String),
+            #              ('ImageId', String),
+            #              ('Image_Path', String),
+            #              ('Width', Int64),
+            #              ('Height', Int64),
+            #              ('Resolution', Int64)])
+            search_results_df = pl.DataFrame(
+                {
+                    "URL": final_links,
+                    "Image_Path": written_files,
+                }
+            )
+            search_results_df = search_results_df.filter(
+                pl.col("Image_Path").is_not_null()
+            )  # drop nulls from the image download
+            search_results_df = search_results_df.with_columns(
+                pl.lit(search_string).alias("SearchString"),
+                pl.lit(class_id).alias("ClassId"),
+                pl.col("Image_Path")
+                .map_elements(lambda x: str(Path(x).name), return_dtype=pl.String)
+                .alias("ImageId"),
+            )
+            query_results.append(search_results_df)
+        driver.close()
+
+    all_results = pl.concat(query_results)
+    num_cpus = psutil.cpu_count(logical=False)
+    if num_cpus > 8:
+        num_cpus = 8
+    print("Analyzing Images")
+    all_results = parallelize_dataframe(all_results, read_image_wrapper, num_cpus)
+
+    result_file_name = f"image_downloads_{timestr}.parquet"
+    reult_file_path = target_directory.parent.joinpath(result_file_name)
+    all_results.write_parquet(
+        reult_file_path, compression="lz4", compression_level=6, statistics=True
+    )
+
+
+if __name__ == "__main__":
+    main()
