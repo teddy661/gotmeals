93acf81fca80a306ebe14ff49d76863cbfff8a26
Melia Soque <meliamks@berkeley.edu>
2024-03-23 12:23:13 +0000

    update

--- flask_app/efficient_net_docker.py
+++ flask_app/efficient_net_docker.py
@@ -1,4 +1,4 @@
-# Use an official Python runtime as a parent image
+# Use an official Python runtime as a parent image- base image for docker container
 FROM python:3.9-slim
 
 # Set the working directory in the container
