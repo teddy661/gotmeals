2844bed16cfc9bc021f42a9e5dea3c34c3d35c40
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-04-02 16:21:25 -0700

    create a parquet file for the hand dataset so we can resample it

---
+++ src/process_hand_dataset.py
@@ -0,0 +1,81 @@
+import argparse
+import gc
+import logging
+import multiprocessing as mp
+import sys
+from datetime import datetime
+from io import BytesIO
+from multiprocessing import Pool
+from pathlib import Path
+
+import cv2
+import numpy as np
+import polars as pl
+import psutil
+from PIL import Image
+
+from utils import *
+
+pc = ProjectConfig()
+
+
+CLASSIFICATION_ROOT = pc.data_root_dir.joinpath("separated_training_data")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Parse separated_training_data dataset"
+    )
+    parser.add_argument(
+        "-f",
+        dest="force",
+        help="force overwrite of existing parquet files",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    prog_name = parser.prog
+    if not CLASSIFICATION_ROOT.exists():
+        logging.error(f"Directory {CLASSIFICATION_ROOT} does not exist.")
+        exit(1)
+    TRAIN_IMG_DIR = CLASSIFICATION_ROOT
+
+    target_parquet_file = pc.data_root_dir.joinpath("separated_training_data.parquet")
+    if target_parquet_file.exists() and not args.force:
+        logging.error(f"File {target_parquet_file} already exists.")
+        exit(1)
+    elif target_parquet_file.exists() and args.force:
+        target_parquet_file.unlink(missing_ok=True)
+
+    num_cpus = psutil.cpu_count(logical=False)
+    if num_cpus > 8:
+        num_cpus = 8
+
+    ClassId = []
+    ImageId = []
+    ImagePath = []
+    directories = [f for f in TRAIN_IMG_DIR.iterdir() if f.is_dir()]
+    for d in directories:
+        files = [f for f in d.iterdir() if f.is_file()]
+        for f in files:
+            ClassId.append(d.name)
+            ImageId.append(f.name)
+            ImagePath.append(str(f))
+    df = pl.DataFrame({"ClassId": ClassId, "ImageId": ImageId, "Image_Path": ImagePath})
+    df = parallelize_dataframe(df, read_image_wrapper, num_cpus)
+    df = df.with_columns(
+        [
+            pl.col("ClassId").alias("original_ClassId"),
+            pl.col("Width").alias("Scaled_Width"),
+            pl.col("Height").alias("Scaled_Height"),
+            pl.col("Resolution").alias("Scaled_Resolution"),
+            pl.col("Image_Path").alias("Scaled_Image_Path"),
+        ]
+    )
+    print(df.head())
+    df.write_parquet(target_parquet_file, compression="snappy")
+
+
+if __name__ == "__main__":
+    mp.freeze_support()
+    mp.set_start_method("spawn")
+    main()
