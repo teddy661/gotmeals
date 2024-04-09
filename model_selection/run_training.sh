da714674114845d42ecabbcca8b70a4bf1fa53ec
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-04-09 06:44:27 -0700

    .\run_training.sh

---
+++ model_selection/run_training.sh
@@ -0,0 +1,6 @@
+scripts = ( baseline_efficientnet_b7.py baseline_efficientnet_v2l.py baseline_efficientnet_v2m.py baseline_vgg19.py fine_tune_efficientnet_b7.py fine_tune_efficientnet_v2l.py fine_tune_efficientnet_v2m.py fine_tune_vgg19.py )
+for script in "${scripts[@]}"; do
+    echo "Running ${script}"
+    python "${script}"
+    echo "Complete ${script}"
+echo "Finished"
