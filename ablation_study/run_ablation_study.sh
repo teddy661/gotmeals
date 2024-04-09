eb78e0b9d4ef178141636757ada5f61ce929d6dd
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-04-09 10:45:55 -0700

    fix mistake

--- ablation_study/run_ablation_study.sh
+++ ablation_study/run_ablation_study.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scripts = "train_efficientnet_v2m_model_01.py train_efficientnet_v2m_model_02.py train_efficientnet_v2m_model_03.py train_efficientnet_v2m_model_04.py train_efficientnet_v2m_model_05.py"
+scripts="train_efficientnet_v2m_model_01.py train_efficientnet_v2m_model_02.py train_efficientnet_v2m_model_03.py train_efficientnet_v2m_model_04.py train_efficientnet_v2m_model_05.py"
 for script in ${scripts}; do 
     echo "Running ${script}"
     python ${script}
