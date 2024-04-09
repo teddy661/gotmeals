68212dd8167339c7623c000a06584d97ebf6a2c7
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-04-09 06:47:22 -0700

    oh boy, that was dumb

--- model_selection/run_training.sh
+++ model_selection/run_training.sh
@@ -1,6 +1,8 @@
-scripts = ( baseline_efficientnet_b7.py baseline_efficientnet_v2l.py baseline_efficientnet_v2m.py baseline_vgg19.py fine_tune_efficientnet_b7.py fine_tune_efficientnet_v2l.py fine_tune_efficientnet_v2m.py fine_tune_vgg19.py )
-for script in "${scripts[@]}"; do
+#!/bin/bash
+scripts="baseline_efficientnet_b7.py baseline_efficientnet_v2l.py baseline_efficientnet_v2m.py baseline_vgg19.py fine_tune_efficientnet_b7.py fine_tune_efficientnet_v2l.py fine_tune_efficientnet_v2m.py fine_tune_vgg19.py" 
+for script in ${scripts}; do
     echo "Running ${script}"
-    python "${script}"
+    python ${script}
     echo "Complete ${script}"
+done
 echo "Finished"
