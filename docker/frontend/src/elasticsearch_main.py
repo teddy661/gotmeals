3fc6be8d4a2d7db731a344714074a474352ef96d
Ed Brown <76535893+teddy661@users.noreply.github.com>
2024-04-15 05:44:15 -0700

    update docker image with latest frontend

--- docker/frontend/src/elasticsearch_main.py
+++ docker/frontend/src/elasticsearch_main.py
@@ -65,11 +65,7 @@ def search_recipes(
     }
 
     # Execute the search query
-    response = es.search(index="recipes", body=query, size=10)
-
-    if not response["hits"]["hits"]:
-        query["query"]["bool"].pop("minimum_should_match", None)
-        response = es.search(index="recipes", body=query, size=10)
+    response = es.search(index="recipes", body=query)
 
     return response
 
